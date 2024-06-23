7dd52e06a59379684eb5dcb626c7ce7eca9fcc96
HarryEddward <he0780070@gmail.com>
2024-06-23 03:20:28 +0200

    changes

--- src/tests/v2/__init__.py
+++ src/tests/v2/__init__.py
@@ -9,13 +9,24 @@ def test_hide_key():
     global gpu
     gpu.hide_key("000102030405060708090a0b0c0d0e0f")
 
-def test_encrypt():
+def test_encrypt_sensible():
     global gpu
     global text_encripted
     text_encripted = gpu.encrypt("Hola este es un mensaje de testeo")
     print(text_encripted)
 
     assert isinstance(text_encripted, str)
+
+def test_encrypt_aes():
+    global gpu
+    global text_encripted
+    text_encripted = gpu.encrypt("Hola este es un mensaje de testeo")
+    print(text_encripted)
+
+    assert isinstance(text_encripted, str)
+
+def test_double_encryption():
+    pass
 
 def test_decypt():
     global gpu
